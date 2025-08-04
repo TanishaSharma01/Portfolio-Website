6410b8b36f38d520194b254bc6e456befd139ec0
Tanisha Sharma <tanishaofficial30@gmail.com>
2025-08-04 10:31:41 +1000

    updated deploy.yml

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,14 @@
-import { Switch, Route, Router } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
-
-function AppRouter() {
-  return (
-    <Router hook={useHashLocation}>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  );
-}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <AppRouter />
+        <Home />
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
