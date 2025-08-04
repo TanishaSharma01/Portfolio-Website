8701bb16e9cbf6339845d27d488efc2469f922f1
Tanisha Sharma <tanishaofficial30@gmail.com>
2025-08-04 10:10:48 +1000

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  base: '/Portfolio-Website/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
